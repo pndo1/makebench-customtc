55c87bd8ff671b615782d32976490a97aa512410
Ryan <Ryan@Ryans-MacBook-Pro.local>
2017-07-22 11:24:34 -0500

    Add mpich compilation support

---
+++ make-mpich.sh
@@ -0,0 +1,26 @@
+if [[ ! -d mpich-$cc$ccver-$libc ]]; then
+  mkdir mpich-$cc$ccver-$libc
+else
+  rm -r mpich-$cc$ccver-$libc
+  mkdir mpich-$cc$ccver-$libc
+fi
+if [[ ! -d build-mpich-$cc$ccver-$libc ]]; then
+  mkdir build-mpich-$cc$ccver-$libc
+  cd build-mpich-$cc$ccver-$libc
+else
+  rm -r build-mpich-$cc$ccver-$libc
+  mkdir build-mpich-$cc$ccver-$libc
+  cd build-mpich-$cc$ccver-$libc/src
+fi
+
+
+
+export CC= $TOOLDIR/bin/$TOOLCHAIN-$cc
+export CFLAGS = -lm -L$TOOLDIR/$TOOLCHAIN/sysroot/lib64/
+export CXXFLAGS = -lm -L$TOOLDIR/$TOOLCHAIN/sysroot/lib64/
+export LDFLAGS = -lm -L$TOOLDIR/$TOOLCHAIN/sysroot/lib64/ -Wl,--dynamic-linker=$LD64SO,-rpath,$TOOLDIR/$TOOLCHAIN/sysroot/lib64
+export LD=$TOOLDIR/bin/$TOOLCHAIN-ld
+export CPP = $TOOLDIR/bin/$TOOLCHAIN-cpp
+export CXX=$TOOLDIR/bin/$TOOLCHAIN-g++
+export LIBS =
+../configure --prefix=$benchpath/mpich-$cc$ccver-$libc --exec-prefix=$benchpath/mpich-$cc$ccver-$libc --enable-shared --with-pm=hydra --with-pmi=yes --enable-romio
