5488e2cc2a8da6257c6b9171baf7d065024fa478
Ryan <Ryan@Ryans-MacBook-Pro.local>
2017-07-22 13:29:22 -0500

    HPCG with MPICH

--- make-hpcg.sh
+++ make-hpcg.sh
@@ -13,18 +13,20 @@ if [ -e "Make.$cc$ccver-$libc" ]
 echo $LD64SO
 echo -e 'SHELL        = /bin/sh\nCD           = cd\n\nCP           = cp\nLN_S         = ln -s -f\nMKDIR        = mkdir -p\nRM           = /bin/rm -f\nTOUCH        = touch' >> Make.$cc$ccver-$libc
 echo -e 'TOPdir       = .\nSRCdir       = $(TOPdir)/src\nINCdir       = $(TOPdir)/src\nBINdir       = $(TOPdir)/bin' >> Make.$cc$ccver-$libc
-echo -e 'MPdir        =\nMPinc        =\nMPlib        =' >> Make.$cc$ccver-$libc
+echo -e "MPdir        = $MPICHPATH/mpich-$cc$ccver-$libc" >> Make.$cc$ccver-$libc
+echo -e "MPinc        = $MPICHPATH/mpich-$cc$ccver-$libc/include" >> Make.$cc$ccver-$libc
+echo -e "MPlib        = $MPICHPATH/mpich-$cc$ccver-$libc/lib" >> Make.$cc$ccver-$libc
 echo -e 'HPCG_INCLUDES = -I$(INCdir) -I$(INCdir)/$(arch) $(MPinc)\nHPCG_LIBS     =' >> Make.$cc$ccver-$libc
-echo -e 'HPCG_OPTS     = -DHPCG_NO_MPI' >> Make.$cc$ccver-$libc
+echo -e 'HPCG_OPTS     = ' >> Make.$cc$ccver-$libc
 echo -e 'HPCG_DEFS     = $(HPCG_OPTS) $(HPCG_INCLUDES)' >> Make.$cc$ccver-$libc
 
 echo "TOOLCHAIN=$TOOLCHAIN" >> Make.$cc$ccver-$libc
 echo "TOOLDIR=$TOOLDIR" >> Make.$cc$ccver-$libc
 echo "LD64SO=$LD64SO" >> Make.$cc$ccver-$libc
 echo "CC=$TOOLDIR/bin/$TOOLCHAIN-$cc" >> Make.$cc$ccver-$libc
-echo "CXX=$TOOLDIR/bin/$TOOLCHAIN-g++" >> Make.$cc$ccver-$libc
+echo "CXX=$MPICHPATH/mpich-$cc$ccver-$libc/bin/mpicxx" >> Make.$cc$ccver-$libc
 #echo "LINKER=$TOOLDIR/bin/$TOOLCHAIN-ld" >> Make.$cc$ccver-$libc
-echo 'CXXFLAGS =  $(HPCG_DEFS) -O3 -ffast-math -ftree-vectorize -ftree-vectorizer-verbose=0 -fopenmp -lm '-L$toollibs/ -Wl,--dynamic-linker=$LD64SO,-rpath,"$TOOLDIR"/"$TOOLCHAIN"/sysroot/lib64 >> Make.$cc$ccver-$libc
+echo 'CXXFLAGS =  $(HPCG_DEFS) -O3 -ffast-math -ftree-vectorize -ftree-vectorizer-verbose=0 -fopenmp -lm '-L$toollibs/ -Wl,--dynamic-linker=$LD64SO,-rpath,"$TOOLDIR"/"$TOOLCHAIN"/sysroot/lib64-rpath,$MPICHPATH/mpich-$cc$ccver-$libc/lib >> Make.$cc$ccver-$libc
 #echo 'LINKFLAGS = $(HPCG_INCLUDES) -lm -lgomp' --dynamic-linker=$LD64SO >> Make.$cc$ccver-$libc
 echo -e 'LINKER       = $(CXX)\nLINKFLAGS    = $(CXXFLAGS)' >> Make.$cc$ccver-$libc
 echo "CPP = $TOOLDIR/bin/$TOOLCHAIN-cpp" >> Make.$cc$ccver-$libc
