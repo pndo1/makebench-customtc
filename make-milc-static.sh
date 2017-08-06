d0d20b23581d20a8c6ce201c21af1cd04943e804
Ryan <Ryan@Ryans-MacBook-Pro.local>
2017-08-06 13:14:46 -0500

    New MILC static

---
+++ make-milc-static.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+cd $benchpath
+
+if [[ ! -d milc-$cc$ccver-$libc ]]; then
+  benchfile=$(ls $benchpath/*.tar.gz | grep -i milc)
+  tar -xf $benchfile
+  benchfolder=$(echo $benchfile | sed -e 's/.tar.gz$//')
+  mv $benchfolder milc-$cc$ccver-$libc
+  cd milc-$cc$ccver-$libc
+else
+  rm -r milc-$cc$ccver-$libc
+  benchfile=$(ls $benchpath/*.tar.gz | grep -i milc)
+  tar -xf $benchfile
+  benchfolder=$(echo $benchfile | sed -e 's/.tar.gz$//')
+  mv $benchfolder milc-$cc$ccver-$libc
+  cd milc-$cc$ccver-$libc
+fi
+echo $LD64SO
+cd libraries
+sed -i "30s|.*|CC = $TOOLDIR/bin/$TOOLCHAIN-$cc|" Make_vanilla
+sed -i "39iLD_LIBRARY_PATH="$TOOLDIR/$TOOLCHAIN/sysroot/usr/lib/ $TOOLDIR/$TOOLCHAIN/sysroot/lib64/"" Make_vanilla
+sed -i "36s|.*|OPT              = -static -O3 -opt-prefetch|" Make_vanilla
+#--dynamic-linker=$LD64SO,-rpath,$TOOLDIR/$TOOLCHAIN/sysroot/lib64,-rpath,$TOOLDIR/$TOOLCHAIN/sysroot/usr/lib/|
+cd ../ks_imp_rhmc
+sed -i "48s|.*|OPT              = -static -g -O3 -Wl,-Bstatic,-lm -Wl,-Bstatic,-lgomp -fopenmp -Wl,-L$TOOLDIR/$TOOLCHAIN/sysroot/lib64/,-L$TOOLDIR/$TOOLCHAIN/sysroot/usr/lib/,-L$MPICHPATH/mpich-$cc$ccver-$libc/lib|" Makefile
+#-Wl,-L$toollibs/,-L$MPICHPATH/mpich-$cc$ccver-$libc/lib,--dynamic-linker=$LD64SO,-rpath,$TOOLDIR/$TOOLCHAIN/sysroot/lib64,-rpath,$TOOLDIR/$TOOLCHAIN/sysroot/usr/lib/,-rpath,$MPICHPATH/mpich-$cc$ccver-$libc/lib
+sed -i "49iOCFLAGS = -tpp2 -static" Makefile
+sed -i "38s|.*|CC = /soft/compilers/experimental/mpich-3.2/mpich-$cc$ccver-$libc/bin/mpicc|" Makefile
+sed -i "40s|.*|CC = /soft/compilers/experimental/mpich-3.2/mpich-$cc$ccver-$libc/bin/mpicc|" Makefile
+sed -i "124iMPdir        = $MPICHPATH/mpich-$cc$ccver-$libc/" Makefile
+sed -i '125iIMPI        = -I$(MPdir)/include' Makefile
+sed -i '126iMPlib        = $(Mpdir)/lib/libmpi.a' Makefile
+sed -i '128iLMPI = -Wl,-Bstatic,-L/$(MPdir)/lib/shared -Wl,-Bstatic,-L/$(MPdir)/lib' Makefile
+sed -i 's|qopenmp|fopenmp|' Makefile
+
+echo "milc-$cc$ccver-$libc has been configured"
+echo "Begin compilation!"
+cd ..
+./build.sh
+echo "Finished with milc!"
+echo "Path to spec:"
+echo "$benchpath/milc-$cc$ccver-$libc/"
+status=2
